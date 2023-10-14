0b3cc3e4f1fc618ca86c76d31ea1d88ca21f9160
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-10-14 13:06:49 +0900

    記事一覧のサムネイル変更

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -13,7 +13,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
         <article className="shadow my-4 flex flex-col">
           <Link href={`articles/${article.id}`} className="hover:opacity-75">
             <Image
-              src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
               alt=""
               width={1280}
               height={300}
